ecb3262c1de7180bd5830cd5c831e6710e7e2307
Ubuntu <ubuntu@ip-172-31-14-119.us-west-1.compute.internal>
2018-12-29 19:03:38 +0000

    Tests debit() with insufficient funds

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -11,3 +11,17 @@ public class TestBankAccount {
     double amount = account.debit(5);
     Assert.assertEquals(5.0, amount);
 }
+@test
+public void testDebitWithInsufficientFunds() {
+    BankAccount account = new BankAccount(10);
+    double amount = account.debit(11);
+    Assert.assertEquals(10.0, amount);
+ }
+}
+
+
+
+
+
+
+
